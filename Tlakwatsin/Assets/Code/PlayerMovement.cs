c9167bf766825b6d140f9c8461eaf2887baac188
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2020-12-07 04:14:04 -0600

    Animaci[on UI

--- Tlakwatsin/Assets/Code/PlayerMovement.cs
+++ Tlakwatsin/Assets/Code/PlayerMovement.cs
@@ -8,47 +8,56 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     public Animator animator;
     public float runSpeed = 40f;
-    float horizontalMove = 0f;       
+    float horizontalMove = 0f;
+    float dashMove = 0;
     bool jump = false;
+  
+   
 
-
-
-
-    // Update is called once per frame
     void Update()
     {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
             animator.SetBool("IsJumping", true);
-
-
         }
-
         else
         {
             return;
+        } 
+
+    }
+
+    void Dash()
+    {
+        dashMove = horizontalMove * 2;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                
+            }
         }
     }
+
 
         public void OnLanding()
         {
             animator.SetBool("IsJumping", false);
-        }
-
-        
+        }        
 	
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);        
         jump = false;
-
     }
+
+
+
+
 }
 
 
